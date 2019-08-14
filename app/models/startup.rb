c98f150b5639b9de3338b1e22dbc7397c6a0ec1e
Josh Nho <joshnho@gmail.com>
2019-08-13 22:03:18 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,62 @@
 class Startup
+    attr_reader :name, :founder, :domain
 
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def self.find_by_founder(founder)
+        self.all.find do |startup|
+            startup.founder == founder
+        end
+    end
+
+    def self.domains
+        self.all.map do |startup|
+            startup.domain
+        end
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select do |fr|
+            fr.startup == self
+        end.count
+    end
+
+    def total_funds
+        FundingRound.all.map do |fr|
+            fr.investment if fr.startup == self
+        end.compact.sum
+    end
+
+    def investors
+        FundingRound.all.map do |fr|
+            if fr.startup == self
+                fr.venture_capitalist
+            end
+        end.uniq.compact # kept returning a nil value for existing                                       venture_capitalists that did not invest
+    end
+
+    def big_investors
+
+    end
 end
+
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# I don't really know what domain, type of investment, etc.. mean in the investment world in real life so I just used any values that I thought would make as much sense as possible and passed them as arguments, just for the sake of testing the codes.
+
+su1 = Startup.new("Nvidia", "Joe", "NY")
+su2 = Startup.new("AMD", "Jason", "NJ")
+su3 = Startup.new("Sony", "Jordan", "CA")
+
+vc1 = VentureCapitalist.new("Josh", 10000)
+vc2 = VentureCapitalist.new("James", 5000)
+vc3 = VentureCapitalist.new("Johnny", 10000000)
+
+fr1 = FundingRound.new(su1, vc1, "cash", 100)
+fr2 = FundingRound.new(su1, vc2, "check", 500)
+fr3 = FundingRound.new(su3, vc1, "credit", 300)
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
